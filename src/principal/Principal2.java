aa02fd42508506020ebc31cc3b608b5a8a16ddda
danisanjub <danisanjub@gmail.com>
2023-01-30 10:38:54 +0100

    cambio1

---
+++ src/principal/Principal2.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
+
+	}
+
+}
